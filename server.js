32ffc4ebac1aae71ad49844af78657db56568c4c
Cepas <sylvain.courroye@gmail.com>
2020-02-05 23:33:52 +0100

    initialisation MongoDB
    ajout du module et des première fonction mongodb

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+const url = "mongodb://localhost:27017/";
+
+/*
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("Talion");
+    var myobj = { name: "Company Inc", address: "Highway 37" };
+    dbo.collection("customers").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  }); 
+*/
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("Talion");
+    dbo.collection("customers").findOne({}, function(err, result) {
+      if (err) throw err;
+      console.log(result.name);
+      db.close();
+    });
+  }); 
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
