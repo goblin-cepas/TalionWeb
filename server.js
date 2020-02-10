f0354b8f6aa214fca4fe9ebef6e33505792f91c8
Cepas <sylvain.courroye@gmail.com>
2020-02-10 09:20:13 +0100

    debut inscription
    debut de la creation de compte, etc...

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
+var fs = require('fs');
 const url = "mongodb://localhost:27017/";
 
 /*
@@ -18,32 +19,41 @@ MongoClient.connect(url, function(err, db) {
   }); 
 */
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+  var dbo = db.db("Talion");
+  dbo.collection("customers").findOne({}, function (err, result) {
     if (err) throw err;
-    var dbo = db.db("Talion");
-    dbo.collection("customers").findOne({}, function(err, result) {
-      if (err) throw err;
-      console.log(result.name);
-      db.close();
-    });
-  }); 
+    console.log(result.name);
+    db.close();
+  });
+});
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/views/index.html');
+  loadPage("index", res);
 })
-    .get('/index', function (req, res) {
-        res.sendFile(__dirname + '/views/index.html');
-    })
-    ;
+  .get('/index', function (req, res) {
+    loadPage("index", res);
+  })
+  .get('/inscription', function (req, res) {
+    loadPage("inscription", res);
+  })
+  ;
 
 io.on('connection', function (socket) {
-    console.log('a user connected');
-    socket.on('connexionUser', function (data) {
-        console.log(data);
-    });
+  console.log('a user connected');
+  socket.on('connexionUser', function (data) {
+    console.log(data);
+  });
 });
 
 http.listen(80, function () {
-    console.log('listening on *:80');
+  console.log('listening on *:80');
 });
 
+function loadPage(page, res) {
+  res.write(fs.readFileSync(__dirname + '/views/header.html', 'utf8'));
+  res.write(fs.readFileSync(__dirname + '/views/' + page + '.html', 'utf8'));
+  res.write(fs.readFileSync(__dirname + '/views/footer.html', 'utf8'));
+  res.end();
+}
